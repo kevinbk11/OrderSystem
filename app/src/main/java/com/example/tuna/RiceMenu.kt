7f50f706793cb7500be4e445ef946fd67ee35722
r91090701 <r91090701@gmail.com>
2020-12-06 21:14:33 +0800

    doing half

--- app/src/main/java/com/example/tuna/RiceMenu.kt
+++ app/src/main/java/com/example/tuna/RiceMenu.kt
@@ -12,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_noodles_menu.*
 
 class RiceMenu : AppCompatActivity(), deleteListItem,send {
     var arr:Array<TextView?> = arrayOf()
+    override var NowList: Array<TextView?> = arrayOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rice_menu)
@@ -19,8 +20,8 @@ class RiceMenu : AppCompatActivity(), deleteListItem,send {
         arr = arrayOf(A1,B1,C1,D1)
         recive(arr,intent)
         toast = Toast.makeText(applicationContext, full, Toast.LENGTH_LONG)
+        updata(arr)
     }
-    override var NowList: Array<TextView?> = arr
     override fun onBackPressed() {
         var intent= Intent(this,MainActivity::class.java)
         SendTextToActivity(A1,B1,C1,D1,intent)
