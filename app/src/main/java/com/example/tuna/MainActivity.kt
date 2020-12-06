909b3722d6fc5c4eb072a7365ecfd6c896e88b0f
r91090701 <r91090701@gmail.com>
2020-12-06 14:20:18 +0800

    d

--- app/src/main/java/com/example/tuna/MainActivity.kt
+++ app/src/main/java/com/example/tuna/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.tuna
 import FoodClass.*
 import MainSystem.CheckNull
 import MainSystem.SendTextToActivity
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import deleteInterface.deleteListItem
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
 
 class MainActivity : AppCompatActivity(),deleteListItem{
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +28,7 @@ class MainActivity : AppCompatActivity(),deleteListItem{
         var c=intent.getStringExtra("C")
         var d=intent.getStringExtra("D")
         CheckNull(A,B,C,D,a,b,c,d)
-        /*val alertDialog = AlertDialog.Builder(this@MainActivity)
+        val alertDialog = AlertDialog.Builder(this@MainActivity)
         alertDialog.setTitle("輸入桌號")
         val input = EditText(this)
         var tableNumber =input.text.toString()
@@ -30,7 +37,15 @@ class MainActivity : AppCompatActivity(),deleteListItem{
                 .setPositiveButton("確定", DialogInterface.OnClickListener { _, _ ->
                 })
 
-        alertDialog.show()*/
+        alertDialog.show()
+        Thread{
+            var Client= Socket("192.168.1.101",5006)
+            val input = Client?.getInputStream()
+            val reader = BufferedReader(InputStreamReader(input))
+            val output=Client.getOutputStream()
+            var writer= PrintWriter(output,true)
+            writer.print(tableNumber.toInt())
+        }
     }
 
     fun D1(view:View)
