0bfe2002706536a0c52fd0f89e99da35a4f76ba1
tuna <a38708242@gmail.com>
2020-12-07 22:24:21 +0800

    endeat page and button

---
+++ app/src/main/java/com/example/tuna/EndEat.kt
@@ -0,0 +1,11 @@
+package com.example.tuna
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EndEat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_eat)
+    }
+}
---
+++ app/src/main/java/com/example/tuna/FoodRecord.kt
@@ -0,0 +1,11 @@
+package com.example.tuna
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FoodRecord : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_record)
+    }
+}
